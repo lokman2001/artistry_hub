55617ee2fb825adada86793b9d1cd70280a43afc
lokman2001 <lukeleon83@gmail.com>
2025-05-23 16:52:05 +0630

    6th

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Header from './component/header'
-import DisplayImg from './component/displayImg'
-import WelcomeSection from './component/welcomeSection'
-import FeaturedArtworksSection from './component/featuredArtworksSection'
-import EventSpace from './component/eventSpace'
-import NewsLetterSpace from './component/NewsLetterSpace'
-import InvitationSpace from './component/InvitationSpace'
-import Event from './component/event'
-import News from './component/News'
-import Invitation from './component/Invitation'
+import { createContext } from 'react'
+import Layout from './layout'
+import Data from "./JsonData/Data.json"
+import Home from './page/Home'
+import About from './page/About'
+import ArtAndArtist from './page/ArtAndArtist'
+import Contact from './page/Contact'
+
+
+export const AppContext = createContext();
 
 function App() {
 
@@ -30,27 +30,10 @@ function App() {
 
   return (
     <>
-      <Header></Header>
-      <main>
-        <DisplayImg />
-        <div className='px-28'>
-          <WelcomeSection />
-          <FeaturedArtworksSection />
-          <EventSpace>
-            <Event />
-          </EventSpace>
-          <NewsLetterSpace>
-            <News />
-          </NewsLetterSpace>
-          <InvitationSpace>
-            <Invitation />
-          </InvitationSpace>
 
-
-        </div>
-
-
-      </main>
+      <AppContext value={{ Data }}>
+        <RouterProvider router={router}></RouterProvider>
+      </AppContext>
 
     </>
   )
--- src/component/Contact/form.jsx
+++ src/component/Contact/form.jsx
@@ -1,16 +1,32 @@
+import { useReducer, useRef } from "react"
+
 export default function Form() {
+
+    const Name = useRef();
+    const Purpose = useRef();
+    const Messages = useRef();
+
+    const Send = () => {
+        let message = {
+            name: Name,
+            purpose: Purpose,
+            message: Messages
+        }
+        console.log(message)
+    }
+
     return (
         <form className=" p-5 my-4   border rounded-xl " id="Form">
             <h1 className="text-2xl border-b mb-5">Contact Box</h1>
             <div className="grid grid-cols-3 gap-3">
                 <div >
                     <label htmlFor="name" className=" mx-3 block">Name</label>
-                    <input type="text" name="name" id="name" placeholder="Name" className=" my-2 border p-2 rounded-xl w-full" />
+                    <input ref={Name} type="text" name="name" id="name" placeholder="Name" className=" my-2 border p-2 rounded-xl w-full" />
                 </div>
 
                 <div>
                     <label htmlFor="purpose" className=" mx-3 block">Purpose</label>
-                    <select name="purpose" id="purpose" className=" my-2 border p-2 rounded-xl w-full">
+                    <select ref={Purpose} name="purpose" id="purpose" className=" my-2 border p-2 rounded-xl w-full">
                         <option className=" text-black" value="Suggestion">Suggestion Box</option>
                         <option className=" text-black" value="Request">Request for join</option>
                         <option className=" text-black" value="Report">Report Box</option>
@@ -18,14 +34,10 @@ export default function Form() {
                 </div>
                 <div>
                     <label htmlFor="message" className=" mx-3 block">Message</label>
-                    <input type="text" name="message" id="message" placeholder="Message" className=" my-2 border p-2 rounded-xl w-full" />
+                    <input ref={Messages} type="text" name="message" id="message" placeholder="Message" className=" my-2 border p-2 rounded-xl w-full" />
                 </div>
-
             </div>
-
-
-
-            <button type="submit" className=" my-4 border p-2 rounded-xl w-full">Send Messages</button>
+            <button type="button" onClick={Send} className=" my-4 border p-2 rounded-xl w-full">Send Messages</button>
 
         </form>
     )
